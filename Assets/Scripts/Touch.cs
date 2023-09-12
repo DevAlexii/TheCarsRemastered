897ff1864e6a4b470a882f8aae2618d304124da1
Entoni1122 <entonishkalla34@gmail.com>
2023-09-12 11:19:05 +0200

    MobileSetUP

--- Assets/Scripts/Touch.cs
+++ Assets/Scripts/Touch.cs
@@ -5,15 +5,14 @@ public class Touch : MonoBehaviour
     void Update()
     {
 #if UNITY_ANDROID
-    if (Input.touchCount > 0)
+        if (Input.touchCount > 0)
         {
             if (Input.touches[0].phase == TouchPhase.Ended)
             {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
                 if (Physics.Raycast(ray, out hit, 100f))
                 {
-                    Debug.Log(hit.transform.gameObject.name);
                     if (hit.transform.TryGetComponent(out Car_Interface clickedCar))
                     {
                         clickedCar.OnCarClicked();
@@ -22,11 +21,13 @@ public class Touch : MonoBehaviour
             }
         }
 #endif
+
+#if UNITY_EDITOR
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit,100f))
+            if (Physics.Raycast(ray, out hit, 100f))
             {
                 if (hit.transform.TryGetComponent(out Car_Interface clickedCar))
                 {
@@ -34,5 +35,8 @@ public class Touch : MonoBehaviour
                 }
             }
         }
+#endif
     }
 }
+
+
