f416b7f6d7446ffc5e5b4d18bce91332e649f6b7
DevAlexii <a.d.s100698@gmail.com>
2023-09-13 10:58:51 +0200

    [Feature] Add Outline for vehicle

--- Assets/Scripts/Car_Script/Car_Random_Model.cs
+++ Assets/Scripts/Car_Script/Car_Random_Model.cs
@@ -11,6 +11,7 @@ public class Car_Random_Model : MonoBehaviour
 
         transform.GetChild(0).GetComponent<MeshFilter>().sharedMesh = random_model.transform.GetChild(4).GetComponentInChildren<MeshFilter>().sharedMesh;
         transform.GetChild(0).GetComponent<MeshRenderer>().sharedMaterials = random_model.transform.GetChild(4).GetComponentInChildren<MeshRenderer>().sharedMaterials;
+        transform.GetChild(0).GetComponent<Outline>().enabled = true;
         Destroy(this);
     }
 }
