0d1ebc192ab97ef3a7ef157d53c2d01ecd509dfd
Entoni1122 <entonishkalla34@gmail.com>
2023-09-14 16:48:33 +0200

    GraphicChangeScript

---
+++ Assets/Scripts/DropDownGrapghic.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class DropDownGrapghic : MonoBehaviour
+{
+    [SerializeField] RenderPipelineAsset assetsLow;
+    [SerializeField] RenderPipelineAsset assetsHigh;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void ChangeGraphicSettings(int var)
+    {
+        if (var == 0)
+        {
+            GraphicsSettings.defaultRenderPipeline = assetsLow;
+        }
+        if (var == 1)
+        {
+            GraphicsSettings.defaultRenderPipeline = assetsHigh;
+
+        }
+    }
+}
