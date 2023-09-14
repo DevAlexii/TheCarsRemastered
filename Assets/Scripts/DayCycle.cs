1aabcceccb6f4064d7cc7595320ab12498f08621
DevAlexii <a.d.s100698@gmail.com>
2023-09-14 16:15:25 +0200

    [Feature] Ambient Curve

--- Assets/Scripts/DayCycle.cs
+++ Assets/Scripts/DayCycle.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class DayCycle : MonoBehaviour
 {
     [SerializeField] private Transform day_origin;
+    [SerializeField] private Vector3 offset_angle;
     [SerializeField] private float angle;
     [SerializeField] private float day_time_speed;
 
+    [SerializeField] private Light sunset;
+    [SerializeField] private AnimationCurve sunset_curve;
 
     void Update()
     {
         angle += Time.deltaTime * day_time_speed;
-        day_origin.eulerAngles = Vector3.right * angle;
+        day_origin.eulerAngles = new Vector3(angle, offset_angle.y, offset_angle.z);
         if (angle >= 360) angle = 0f;
+        sunset.intensity = sunset_curve.Evaluate(angle);
     }
 }
