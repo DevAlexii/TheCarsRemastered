053f93f2c3acf17b8df936e749585a32f6190b7c
Entoni1122 <entonishkalla34@gmail.com>
2023-09-20 12:12:58 +0200

    Fixes

--- Assets/Scripts/PowerUp/PowerUpBase.cs
+++ Assets/Scripts/PowerUp/PowerUpBase.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
 
-public class PowerUpBase : MonoBehaviour,I_Interface
+public class PowerUpBase : MonoBehaviour, I_Interface
 {
     [SerializeField] private GameObject effect;
 
+    private void Start()
+    {
+        Destroy(this.gameObject, 3f);
+
+    }
     public virtual void OnClicked()
     {
         Destroy(this.gameObject);
