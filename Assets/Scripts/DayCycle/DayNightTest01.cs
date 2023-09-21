19864d2557909deb6314006b0802f93a3ca1eb8e
Entoni1122 <entonishkalla34@gmail.com>
2023-09-21 14:06:44 +0200

    [DayCycle]OptimizationFixes
    Ho cambiato il day_cycle facendo che invece di usare 4 luci dio cane , ne usassimo una alla volta , il sun quando è giorno  e la moon quando è notte , cosi da risparmiare un sacco sulle prestazioni .

---
+++ Assets/Scripts/DayCycle/DayNightTest01.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using Codice.CM.Client.Differences.Graphic;
+using UnityEngine;
+
+public class DayNightTest01 : MonoBehaviour
+{
+    [Header("Day_Varaiables")]
+    public float time;
+    public float dayLenght;
+    public float startday;
+    public Vector3 noon;
+    private float timeRate;
+
+    [Header("Sun")]
+    [SerializeField] Light sun;
+    [SerializeField] AnimationCurve sunIntensity;
+    public Gradient sunGradient;
+
+    [Header("Moon")]
+    [SerializeField] Light moon;
+    [SerializeField] AnimationCurve moonintesnity;
+    public Gradient moonGradient;
+
+    [SerializeField] AnimationCurve intensity_multiplier;
+    [SerializeField] AnimationCurve reflexion_multiplier;
+
+    void Start()
+    {
+        timeRate = 1f / dayLenght;
+
+        time = startday;
+    }
+
+    void Update()
+    {
+        time += timeRate * Time.deltaTime;
+        if (time >= 1f)
+        {
+            time = 0f;
+        }
+        SetLighting();
+        SetSunMoonintensity();
+        SetSunMoonActive();
+    }
+    void SetLighting()
+    {
+        RenderSettings.ambientIntensity = intensity_multiplier.Evaluate(time);
+        RenderSettings.reflectionIntensity = reflexion_multiplier.Evaluate(time);
+    }
+    void SetSunMoonintensity()
+    {
+        sun.intensity = sunIntensity.Evaluate(time);
+        moon.intensity = moonintesnity.Evaluate(time);
+
+        sun.color = sunGradient.Evaluate(time);
+        moon.color = moonGradient.Evaluate(time);
+    }
+    void SetSunMoonActive()
+    {
+        if (sun.intensity == 0 && sun.gameObject.activeInHierarchy)
+        {
+            sun.gameObject.SetActive(false);
+
+        }
+        else if (sun.intensity > 0 && !sun.gameObject.activeInHierarchy)
+        {
+            sun.gameObject.SetActive(true);
+
+        }
+        if (moon.intensity == 0 && moon.gameObject.activeInHierarchy)
+        {
+            moon.gameObject.SetActive(false);
+
+        }
+        else if (moon.intensity > 0 && !moon.gameObject.activeInHierarchy)
+        {
+            moon.gameObject.SetActive(true);
+        }
+    }
+}
