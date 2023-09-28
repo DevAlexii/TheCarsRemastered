1bf6889e02f8cf4904afebeae5095542119e0968
Entoni1122 <entonishkalla34@gmail.com>
2023-09-28 16:33:11 +0200

    HearthVisual

--- Assets/Scripts/PowerUp/PowerUp_Health.cs
+++ Assets/Scripts/PowerUp/PowerUp_Health.cs
@@ -5,6 +5,8 @@ public class PowerUp_Health : PowerUpBase
     public override void OnClicked()
     {
         GameManager.self.UpdateLife(1);
+        GameObject effect = Instantiate(this.effect, transform.position, Quaternion.identity);
+        Destroy(effect, 1.5f);
         base.OnClicked();
     }
 }
