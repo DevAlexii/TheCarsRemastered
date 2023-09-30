4cdad7b3ec4c3583110230e33f32b54ab6b4ebfd
DevAlexii <a.d.s100698@gmail.com>
2023-09-30 18:51:38 +0200

    Update SizeAnimationRef

--- Assets/Scripts/Test/SizeAnimationEditor.cs
+++ Assets/Scripts/Test/SizeAnimationEditor.cs
@@ -7,6 +7,14 @@ public class SizeAnimationEditor : Singleton<SizeAnimationEditor>
     public AnimationCurve Size_curve;
     public float Time;
 
+    [ExecuteAlways]
+    private void Start()
+    {
+        Destroy(GetComponent<MeshFilter>());
+        Destroy(GetComponent<MeshRenderer>());
+    }
+
+
     void Update()
     {
         transform.localScale = Vector3.one * Size_curve.Evaluate(Timer);
