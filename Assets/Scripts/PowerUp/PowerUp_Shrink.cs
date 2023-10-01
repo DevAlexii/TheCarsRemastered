de8a973a5a0b0a61c90abe4165c060014f8cd519
Ntn28 <ntncara016@gmail.com>
2023-10-01 15:25:58 +0200

    .

--- Assets/Scripts/PowerUp/PowerUp_Shrink.cs
+++ Assets/Scripts/PowerUp/PowerUp_Shrink.cs
@@ -5,7 +5,7 @@ public class PowerUp_Shrink : PowerUpBase
     public override void OnClicked()
     {
         Car_Manager.self.ToggleShrink();
-        GameObject effect = Instantiate(this.effect, transform.position, Quaternion.identity);
+        GameObject effect = Instantiate(this.effect, transform.position - new Vector3(0f, 0.5f, 0f), Quaternion.identity);
         Destroy(effect, 1f);
         base.OnClicked();
     }
