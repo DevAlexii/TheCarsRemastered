37f5bb61ff4c0a86d89d5850057c7603898546c7
DevAlexii <a.d.s100698@gmail.com>
2024-01-31 17:01:50 +0100

    Entoni e' un nabbo donna

--- Assets/Scenes/UI_Script/ShowStats.cs
+++ Assets/Scenes/UI_Script/ShowStats.cs
@@ -3,20 +3,10 @@ using TMPro;
 
 public class ShowStats : MonoBehaviour
 {
-    [SerializeField] TMP_Text Ram;
     [SerializeField] TMP_Text Fps;
-    [SerializeField] TMP_Text deviceType;
-    [SerializeField] TMP_Text deviceName;
-    [SerializeField] TMP_Text deviceModel;
+   
     private float deltaTime;
 
-    private void Start()
-    {
-        Ram.text = "Device Ram : " + SystemInfo.systemMemorySize.ToString();
-        deviceType.text = "Device Type : " + SystemInfo.deviceType.ToString();
-        deviceName.text = "Device Name : " + SystemInfo.deviceName.ToString();
-        deviceModel.text = "Device Model : " + SystemInfo.deviceModel.ToString();
-    }
     void Update()
     {
         deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
--- Assets/Scripts/Input/Touch.cs
+++ Assets/Scripts/Input/Touch.cs
@@ -6,44 +6,45 @@ public class Touch : MonoBehaviour
     private LayerMask layer;
     void Update()
     {
-        //#if UNITY_ANDROID
-        //        if (Input.touchCount > 0)
-        //        {
-        //            if (Input.touches[0].phase == TouchPhase.Ended)
-        //            {
-        //                RaycastHit hit;
-        //                Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
-        //                if (Physics.Raycast(ray, out hit, 100f,layer))
-        //                {
-        //                    if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Default") && GameManager.self.doOnce)
-        //                    {
-        //                        return;
-        //                    }
-        //                    if (hit.transform.TryGetComponent(out I_Interface clickedCar))
-        //                    {
-        //                        clickedCar.OnClicked();
-        //                    }
-        //                }
-        //            }
-        //        }
-        //#endif
-#if UNITY_EDITOR_WIN
-        if (Input.GetMouseButtonUp(0))
+#if UNITY_ANDROID
+        if (Input.touchCount > 0)
         {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit, 100f, layer))
+            if (Input.touches[0].phase == TouchPhase.Ended)
             {
-                if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Default") && GameManager.self.doOnce)
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+                if (Physics.Raycast(ray, out hit, 100f, layer))
                 {
-                    return;
-                }
-                if (hit.transform.TryGetComponent(out I_Interface clickedCar))
-                {
-                    clickedCar.OnClicked();
+                    if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Default") && GameManager.self.doOnce)
+                    {
+                        return;
+                    }
+                    if (hit.transform.TryGetComponent(out I_Interface clickedCar))
+                    {
+                        clickedCar.OnClicked();
+                    }
                 }
             }
         }
 #endif
+        //#if UNITY_EDITOR_WIN
+        //        if (Input.GetMouseButtonUp(0))
+        //        {
+        //            RaycastHit hit;
+        //            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //            if (Physics.Raycast(ray, out hit, 100f, layer))
+        //            {
+        //                if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Default") && GameManager.self.doOnce)
+        //                {
+        //                    return;
+        //                }
+        //                if (hit.transform.TryGetComponent(out I_Interface clickedCar))
+        //                {
+        //                    clickedCar.OnClicked();
+        //                }
+        //            }
+        //        }
+        //#endif
+        //    }
     }
 }
